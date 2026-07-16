5de453de95cfa405984179104511c2a4548e4a9c
AGBellerive <AGBellerive@gmail.com>
2026-07-16 19:51:45 -0400

    Pushed from wrong dir

--- web_server/server.py
+++ web_server/server.py
@@ -9,8 +9,12 @@ app = Flask(__name__)
 file_lock = Lock()
 
 # Loading config file
-with open('config.json', 'r', encoding='utf-8') as f:
+# with open('config.json', 'r', encoding='utf-8') as f:
+#     config_file = json.load(f)
+
+with open('C:/Users/kokaw/Desktop/Projects/PasswordManager/web_server/config.json', 'r', encoding='utf-8') as f:
     config_file = json.load(f)
+
 
 PASSWORD_FILE = config_file["PASSWORD_PATH"]
 PORT = config_file["PORT"]
